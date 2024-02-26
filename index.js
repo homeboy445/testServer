8aab2d5f33c8c092e69d19420b6af60d1b60df02
Akshit Kushwaha <akshit.kushwaha@wingify.com>
2024-02-26 17:11:07 +0530

    Added script

--- index.js
+++ index.js
@@ -36,6 +36,14 @@ app.get("/", (req, res) => {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     ${smartCode}
+    <script type="text/javascript">
+        window.VWO = window.VWO || [];
+        VWO.push(['config', {
+            storage: {
+                syncUrl: 'https://testserver-mzm3.onrender.com/sync'
+            }
+        }]);
+    </script>
     </head>
     <body class="w3-content" style="max-width:1300px">
     
